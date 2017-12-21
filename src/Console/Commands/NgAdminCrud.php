f5b4fdd4a90582d4a15f5233bfdcd74d7aae66ff
aurelle Meless <aurelle.meless@gmail.com>
2017-12-21 16:16:02 +0000

    fixing class name error

--- src/Console/Commands/NgAdminCrud.php
+++ src/Console/Commands/NgAdminCrud.php
@@ -5,7 +5,7 @@ namespace Emniis\NgAdmin\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
 
-class MakeNgAdminCrud extends Command
+class NgAdminCrud extends Command
 {
   /**
    * The name and signature of the console command.
